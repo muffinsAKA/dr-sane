80de41ec5241945ba3246079fc5b6d51eb6bd6c1
muffinsAKA <muffins@gmail.com>
2023-06-01 03:04:49 -0400

    cleanup

--- index.js
+++ index.js
@@ -363,7 +363,7 @@ async function episode(questionText) {
   await world.createWorld();
 
   // Intro creation
-  await initIntro(camIntro, lottieLoader, rendererIntro, canvasWidth, canvasHeight, canvas, intro);
+  //await initIntro(camIntro, lottieLoader, rendererIntro, canvasWidth, canvasHeight, canvas, intro);
   
   console.log(world)
   // Set first run as complete
