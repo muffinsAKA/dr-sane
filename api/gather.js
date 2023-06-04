c4e0c5d1ee9cdfcd6ce7618addd8fbcb2c3a5795
muffinsAKA <muffins@gmail.com>
2023-06-04 03:49:14 -0400

    frasier

--- api/gather.js
+++ api/gather.js
@@ -70,7 +70,7 @@ export async function gather(questionText, userName) {
     const gptHandoff = {
       "text": `${gptScript}`,
       "voice_settings": {
-        "stability": 0.26,
+        "stability": 0.28,
         "similarity_boost": 1
       }
     }
--- index.js
+++ index.js
@@ -334,7 +334,7 @@ async function adjustSize() {
 
   canvasWidth = window.innerWidth * 0.5;
   canvasHeight = window.innerHeight * 0.6;
-  
+
   renderer.setSize(canvasWidth, canvasHeight);
   
   current.camera.aspect = canvasWidth / canvasHeight;
